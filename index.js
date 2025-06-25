0cb3537207859e983a2cc50797aec3c35f450e59
Anumitha-369 <anumithaj1369@gmail.com>
2025-06-25 23:39:44 +0530

    web-dev-internship-fundamentals

---
+++ index.js
@@ -0,0 +1,86 @@
+/*using document.getElementById to return the value */
+const btn = document.getElementById("submit");
+console.log(btn);
+
+// /* print statement */
+// console.log("Hello World!");
+
+// /* addition */
+// let num1=10;
+// let num2=20;
+// let sum = num1+num2;
+// console.log("The sum of", num1, "and",num2,"is", sum);
+
+// /* division */
+// let num3=10;
+// let num4=20;
+// let div = num1 % num2;
+// console.log("The division of", num3, "and",num4,"is", div);
+
+// /* document.write */
+
+// document.write("Main title");
+
+/* LOOPING STATEMENTS */
+/* if-else */
+
+// let age=20;
+// if(age>=18){
+//     console.log(" You are eligible to vote");
+// }
+// else{
+//     console.log("You are not eligible to vote");
+// }
+
+/* for loop 1 */
+
+for(let i=0; i<=5; i++){
+     console.log("The value of i is: " + i);
+ }
+
+/* for loop 2 */
+
+// let result = "The values from 0 to 5 are: ";
+// for (let i = 0; i <= 5; i++) {
+//     result += i + " ";
+// }
+// console.log(result);
+
+/* while loop */
+
+// let j = 5;
+// while(j <= 10){
+//     console.log("The value is less than or equal to 10: " + j);
+//     j++;
+// }
+
+/* switch-case statements */
+
+// let day=2; // input day of the week (1-7)
+// let day_name ;
+// switch(day){
+//     case 1:
+//         day_name="monday";
+//         break;
+//     case 2 :
+//         day_name="tuesday";
+//         break;  
+//     case 3:                                                                 
+//         day_name="wednesday";
+//         break;
+//     case 4:
+//         day_name="friday";
+//         break;
+//     default:
+//         day_name="invalid day";
+//         break;
+// }
+
+// console.log("Today is " + day_name);
+
+/* function declaration with forms */
+
+
+
+
+
